15cf10287890bf67708353724924a538baa8bb0a
Christopher Lum <lum@uw.edu>
2024-07-08 16:01:26 -0700

    Updating for new data

--- Apps/YouTubeAnalysis/Analysis/Step01_CombineInputs.m
+++ Apps/YouTubeAnalysis/Analysis/Step01_CombineInputs.m
@@ -17,8 +17,8 @@ tic
 %% User selections
 %Input file(s)
 videoInfoFile   = [ReturnPathStringNLevelsUp(1),'\VideoInfo.xlsx'];
-tableDataFile   = [ReturnPathStringNLevelsUp(1),'\Content 2010-09-01_2023-09-08 Christopher Lum\Table data.csv'];
-classificationInUse = 2;    %which classification column to use for groupings
+tableDataFile   = [ReturnPathStringNLevelsUp(1),'\Content 2010-09-01_2024-07-08 Christopher Lum\Table data.csv'];
+classificationInUse = 2;    %which classification column to use for groupings.  1 = full categories, 2 = education/other
 
 %Output file(s)
 augmentedTableDataFile  = 'Step01_TableDataAugmented.xlsx';
@@ -73,6 +73,41 @@ end
 %Augment tableDataTable
 tableDataTable.Classification   = Classification;
 tableDataTable.VideoDuration    = VideoDuration;
+
+%% Handle edge cases
+for k=1:M
+    row = tableDataTable(k,:);
+    
+    if(isnan(row.Views))
+        colIdx = find(strcmp(row.Properties.VariableNames,'Views')==1);
+        tableDataTable(k,colIdx) = {0};
+    end
+
+    if(isnan(row.WatchTime_hours_))
+        colIdx = find(strcmp(row.Properties.VariableNames,'WatchTime_hours_')==1);
+        tableDataTable(k,colIdx) = {0};
+    end
+
+    if(isnan(row.Subscribers))
+        colIdx = find(strcmp(row.Properties.VariableNames,'Subscribers')==1);
+        tableDataTable(k,colIdx) = {0};
+    end
+
+    if(isnan(row.EstimatedRevenue_USD_))
+        colIdx = find(strcmp(row.Properties.VariableNames,'EstimatedRevenue_USD_')==1);
+        tableDataTable(k,colIdx) = {0};
+    end
+
+    if(isnan(row.Impressions))
+        colIdx = find(strcmp(row.Properties.VariableNames,'Impressions')==1);
+        tableDataTable(k,colIdx) = {0};
+    end
+
+    if(isnan(row.ImpressionsClick_throughRate___))
+        colIdx = find(strcmp(row.Properties.VariableNames,'ImpressionsClick_throughRate___')==1);
+        tableDataTable(k,colIdx) = {0};
+    end
+end
 
 %% Write output table
 if(isfile(augmentedTableDataFile))
